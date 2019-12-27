f5b84b8a2de3bb92f7442afcab3ddb7d5e7b1435
kkisic <amareamo.pxhxc@gmail.com>
2019-12-27 15:25:23 +0900

    add Area.vue

---
+++ src/components/Area.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <span>
+      {{ area.name }}
+    </span>
+    <div v-for="key in range(area.startMissionId, area.endMissionId)" :key="key">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Area',
+  props: ['area']
+}
+</script>
+
+<style>
+
+</style>
